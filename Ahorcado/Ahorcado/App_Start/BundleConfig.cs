f233eaeb72b7a98a1d998f0a65ca6e1fb93c46c7
Daniel Murillo <osacr6@gmail.com>
2025-06-11 19:07:09 -0600

    Actividades

--- Ahorcado/Ahorcado/App_Start/BundleConfig.cs
+++ Ahorcado/Ahorcado/App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace Ahorcado
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new Bundle("~/Content/js").Include(
+                      "~/Scripts/ahorcado.js"));
         }
     }
 }
